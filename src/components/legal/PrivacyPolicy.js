c677f92b97cee9783de05be6d2b5e6463fc71e79
vincentc48 <vincent.cai48@gmail.com>
2021-03-17 19:51:40 -0400

    update privacy policy

--- src/components/legal/PrivacyPolicy.js
+++ src/components/legal/PrivacyPolicy.js
@@ -22,11 +22,6 @@ export default function PrivacyPolicy() {
         for visitors to our website with regards to the information that they
         shared and/or collect in Aspyere. This policy is not applicable to any
         information collected offline or via channels other than this website.
-        Our Privacy Policy was created with the help of the{" "}
-        <a href="https://www.privacypolicygenerator.info/#wizard">
-          Free Privacy Policy Generator
-        </a>
-        .
       </p>
 
       <h2>Consent</h2>
@@ -86,6 +81,16 @@ export default function PrivacyPolicy() {
         that is personally identifiable. The purpose of the information is for
         analyzing trends, administering the site, tracking users' movement on
         the website, and gathering demographic information.
+      </p>
+
+      <h2>Cookies and Web Beacons</h2>
+
+      <p>
+        Aspyere uses cookies. These cookies are used to store information
+        including visitors' preferences, and the pages on the website that the
+        visitor accessed or visited. The information is used to optimize the
+        users' experience by customizing our web page content based on visitors'
+        browser type and/or other information.
       </p>
 
       <h2>Advertising Partners Privacy Policies</h2>
